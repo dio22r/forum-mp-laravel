69abfb79e44ad1f87e634cc0bb8921d2a8faae05
dio22r <dio.22ratar@gmail.com>
2022-04-04 10:43:22 +0800

    fix: navbar

--- resources/views/panel/navbar.blade.php
+++ resources/views/panel/navbar.blade.php
@@ -1,8 +1,8 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-navbar shadow">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="{{ route('home') }}">
+    <a class="navbar-brand ps-3 d-flex align-middle" href="{{ route('home') }}">
         <img src="{{ url('/assets/img/logo-gpdi.png') }}" width="22px" />
-        {{ config('app.name', 'Laravel') }}
+        <span class="ms-md-3 ms-1">{{ config('app.name', 'Laravel') }}</span>
     </a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
@@ -14,18 +14,20 @@
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user fa-fw"></i> {{ Auth::user()->name }}
+                <i class="fas fa-cog fa-fw"></i>
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                 <li>
-                    <a class="dropdown-item" href="{{ route('account') }}">My Account</a>
+                    <a class="dropdown-item" href="{{ route('account') }}">
+                        <i class="fas fa-user fa-fw"></i> {{ Auth::user()->name }}
+                    </a>
                 </li>
                 <li>
                     <hr class="dropdown-divider" />
                 </li>
                 <li>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
+                    <a class="dropdown-item text-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
                     </a>
 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
